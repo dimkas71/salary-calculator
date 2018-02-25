bb068581202d8905a29f0691298354abe17c332d
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-02-25 14:28:03 +0200

    Initial commit

---
+++ src/main/java/ua/compservice/compservicesalarycalculator/CompserviceSalaryCalculatorApplication.java
@@ -0,0 +1,12 @@
+package ua.compservice.compservicesalarycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompserviceSalaryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompserviceSalaryCalculatorApplication.class, args);
+	}
+}
